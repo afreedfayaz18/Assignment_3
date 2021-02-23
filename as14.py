7773cfbadee8e0c967b6968ade4327a78a4d989c
Muhammed Afreed Fayas <79002471+afreedfayaz18@users.noreply.github.com>
2021-02-23 14:06:27 +0530

    Add files via upload

---
+++ as14.py
@@ -0,0 +1,17 @@
+def search(arr, n, x):
+ 
+    for i in range(0, n):
+        if (arr[i] == x):
+            return i
+    return -1
+arr = [2, 3, 4, 10, 40]
+print(arr)
+x =0
+x=int(input("Enter the element want to searach :"))
+n = len(arr)
+
+result = search(arr, n, x)
+if(result == -1):
+    print("Element is not present in array")
+else:
+    print("Element is present at index", result)
